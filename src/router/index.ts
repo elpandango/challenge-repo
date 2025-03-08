8eb04383ec9dbb3d3368a337652bdf7a9bb67836
Eugen Radchenko <elpandango@gmail.com>
2025-03-08 19:04:02 +0100

    Routing fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "start",
-      redirect: "/protokoll",
+      component: () => import("../pages/TextPage/TextPage.vue"),
     },
     {
       path: "/protokoll",
