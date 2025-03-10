0812f9045196ba3af9022240b4debfe71b571040
Eugen Radchenko <elpandango@gmail.com>
2025-03-10 06:17:45 +0100

    Redirect in router->index.ts added

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "start",
-      component: () => import("../pages/TextPage/TextPage.vue"),
+      redirect: "/protokoll",
     },
     {
       path: "/protokoll",
